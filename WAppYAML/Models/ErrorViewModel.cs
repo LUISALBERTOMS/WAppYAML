d0ffdaf96fa9e4c2df67c0eea74de6e1c283e829
LUISALBERTOMS <luisalbertoms0312@gmail.com>
2021-02-28 22:12:30 -0500

    Create a repo with ASP.NET CORE 3.1

---
+++ WAppYAML/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WAppYAML.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
